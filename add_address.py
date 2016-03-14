1c826f3e83674ad41c856f2ef5edbf65e6aabc41
Henrique Nogueira <henrique.nogueira@eldorado.org.br>
2016-03-14 18:57:19 -0300

    Added file with cities listing and changed from Google API to Bing API.

--- add_address.py
+++ add_address.py
@@ -1,28 +1,26 @@
 import os
+from json import loads
 
 from decouple import config
 
 BASE_DIR = os.path.dirname(__file__)
-GOOGLE_API_KEY = config('GOOGLE_API_KEY', default='')
+BING_API_KEY = config('BING_API_KEY')
 
 # Making sure the correct request URL is set, according to
-# the presence of the Google API key.
-if GOOGLE_API_KEY is not None:
-    BASE_URL = 'http://maps.google.com/maps/api/geocode/json?address={}&sensor=false&key={}'
-else:
-    BASE_URL = 'http://maps.google.com/maps/api/geocode/json?address={}&sensor=false&key={}'
+# the presence of the Bing API key.
+BASE_URL = 'http://dev.virtualearth.net/REST/v1/Locations?query={}&includeNeighborhood=0&include=queryParse,ciso2&maxResults=10&key={}'
 
 def get_request_url(address):
     """
     Formats the request URL according to the presence
     of the Google API key.
     """
-    if GOOGLE_API_KEY is None:
+    if BING_API_KEY is None:
         return BASE_URL.format(address)
-    return BASE_URL.format(address, GOOGLE_API_KEY)
+    return BASE_URL.format(address, BING_API_KEY)
 
 def main():
-    print(GOOGLE_API_KEY)
+    pass
 
 if __name__ == '__main__':
     main()
